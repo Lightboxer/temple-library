c2b14c4ab7a687e3898b6dca81670168d693dfcf
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2020-07-01 11:00:55 +0000

    plswork

--- backend/src/crypto.ts
+++ backend/src/crypto.ts
@@ -1,3 +1,6 @@
 // Contains functions related to password hashing and encryption for use in io.ts
 
-import { randomBytes, 
+import { randomBytes, createCipheriv, createDecipheriv, createHmac } from 'crypto'
+import { EncryptedData } from 'tinystock-models'
+
+const encryptionAlgorithm = 'a
