ac557ecdf2ca7d7ab8957faa19725cb138ffbdf7
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2020-07-05 14:03:47 +0000

    PR suggestions

--- backend/src/crypto.ts
+++ backend/src/crypto.ts
@@ -9,4 +9,6 @@ const hashingAlgorithm = 'sha256'
 export function encrypt(data: string, passwordHash: string) {
     passwordHash = passwordHash.slice(32) // Only the second half of hash is actually used for encryption - this is a limitation of the AES256 algorithm
     const iv = randomBytes(16)
-    const cipher = createCipheriv(encryptionAlgorithm, passwo
+    const cipher = createCipheriv(encryptionAlgorithm, passwordHash, iv)
+    const encrypted = Buffer.concat([cipher.update(data), cipher.final()])
+    return new EncryptedData(iv.toString('hex'),
