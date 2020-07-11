257e1fbafea9ab0baff993ec451888bd8c87aee5
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2020-07-11 22:04:37 +0000

    Update sample data

--- backend/src/events.ts
+++ backend/src/events.ts
@@ -1,4 +1,5 @@
 // Contains functions directly accessed by main.ts
 // References functions from funcs.ts
 
-import { configure, addItem, findItem, addTransaction, editItem, deleteItem, changePassword, importData, i
+import { configure, addItem, findItem, addTransaction, editItem, deleteItem, changePassword, importData, items, transactions } from './funcs'
+import { AppError, AppErrorCodes, instanceOfItem, instanceOfAdjustment, instanceOfItem
