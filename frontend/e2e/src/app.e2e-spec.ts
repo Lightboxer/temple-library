5fe384e70ca5a0aed1a82f13fea648393f18ec3b
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2020-09-29 20:23:32 +0000

    heap

--- frontend/e2e/src/app.e2e-spec.ts
+++ frontend/e2e/src/app.e2e-spec.ts
@@ -10,3 +10,8 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
+    expect(page.getTitleText()).toEqual('frontend app is running!');
+  });
+
+  afterEach(async () => {
+    // Assert that
