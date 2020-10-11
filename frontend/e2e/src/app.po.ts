e711c75214cb1d2813c4699b9cd6a74df22c8972
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2020-10-11 15:44:13 +0000

    Corrected typos

--- frontend/e2e/src/app.po.ts
+++ frontend/e2e/src/app.po.ts
@@ -6,4 +6,6 @@ export class AppPage {
   }
 
   getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Pr
+    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  }
+}
