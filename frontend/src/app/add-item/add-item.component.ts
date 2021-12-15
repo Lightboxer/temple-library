c21e74f223676126eeaaea2d9607fd0bd9d948ba
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2021-12-15 08:22:25 +0000

    Version 0.2.8

--- frontend/src/app/add-item/add-item.component.ts
+++ frontend/src/app/add-item/add-item.component.ts
@@ -1,2 +1,4 @@
 import { Component, ElementRef, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
-import { For
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Location } from '@angular/common';
+import { ApiService } from
