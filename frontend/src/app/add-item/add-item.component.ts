6b7e837ae14f4c981d83a84d8d57e72e4fca6303
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2021-12-16 22:22:06 +0000

    Updated landing page

--- frontend/src/app/add-item/add-item.component.ts
+++ frontend/src/app/add-item/add-item.component.ts
@@ -5,3 +5,8 @@ import { ApiService } from '../services/api.service';
 import { ErrorService } from '../services/error.service';
 
 @Component({
+  selector: 'app-add-item',
+  templateUrl: './add-item.component.html',
+  styleUrls: ['./add-item.component.scss']
+})
+export class Add
