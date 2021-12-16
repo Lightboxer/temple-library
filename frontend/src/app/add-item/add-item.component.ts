7ece3a0878f8e3515e36b230cef9c3b2ea34e7da
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2021-12-16 10:25:38 +0000

    locks: allow __break_lease to sleep even when break_time is 0

--- frontend/src/app/add-item/add-item.component.ts
+++ frontend/src/app/add-item/add-item.component.ts
@@ -1,4 +1,7 @@
 import { Component, ElementRef, Input, OnInit, Output, ViewChild, EventEmitter } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
-import { ApiService } from
+import { ApiService } from '../services/api.service';
+import { ErrorService } from '../services/error.service';
+
+@Component({
