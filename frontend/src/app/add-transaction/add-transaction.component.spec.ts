b0e586b858f354949f611c4770a9b51a31a834e8
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-02-10 14:35:41 +0000

    Updated comments.

--- frontend/src/app/add-transaction/add-transaction.component.spec.ts
+++ frontend/src/app/add-transaction/add-transaction.component.spec.ts
@@ -1,3 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddTransactionComponent } from './add-transaction.
+import { AddTransactionComponent } from './add-transaction.component';
+
+describe('AddTransactionComponent', () => {
+  let component: AddTransactionComponent;
+  let fixture: ComponentFixture<AddTransac
