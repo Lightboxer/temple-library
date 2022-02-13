d14c564daa9e50fbbfe3f5a212fff17df365841d
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-02-13 21:04:25 +0000

    better cleanup

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,4 +3,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { AddItemComponent } from './add-item/add-item.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { EditItemComponent } from './edit-item/edit-item.component';
-import { HomeCompo
+import { HomeComponent } from './home/home.component';
+import { ItemsComponent } from './items/items.component';
+import { AddTransactionComponent } from './add
