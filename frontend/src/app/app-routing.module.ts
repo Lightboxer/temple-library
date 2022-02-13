74d3c1523d8ec39c08691bfee3ebdbd33ddf4c26
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-02-13 08:19:53 +0000

    enable caching

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,4 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AddItemComponent } from './add-item/add-item.component';
-import { ConfigurationComponent } from './configuration/co
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { EditItemComponent } from './edit-item/edit-item.component';
+import { HomeCompo
