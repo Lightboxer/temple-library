8c0f6c1c5fa6372347df02eb1d3d0def921f95c4
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-03-02 14:38:49 +0000

    3.0.7

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -60,3 +60,73 @@ export class AppComponent implements OnInit, OnDestroy {
           this.setTheme('darkTheme')
           break;
         case themes.lightTheme:
+          this.setTheme('lightTheme')
+          break;
+        case themes.responsiveTheme:
+          this.setTheme('responsiveTheme')
+          break;
+        default:
+          break;
+      }
+    }))
+    this.themeService.loadTheme()
+    this.keyboardShortcutService.addShortcut({ keys: 'Alt./' }).subscribe((res) => {
+      this.showShortcuts()
+    })
+    this.keyboardShortcutService.addShortcut({ keys: 'Alt.arrowleft' }).subscribe((res) => {
+      this.back()
+    })
+    this.keyboardShortcutService.addShortcut({ keys: 'Alt.arrowright' }).subscribe((res) => {
+      this.forward()
+    })
+    this.keyboardShortcutService.addShortcut({ keys: 'Alt.t' }).subscribe((res) => {
+      switch (this.themeService.themeSource.value) {
+        case themes.lightTheme:
+          this.themeService.updateTheme(themes.darkTheme)
+          break;
+        case themes.darkTheme:
+          this.themeService.updateTheme(themes.responsiveTheme)
+          break;
+        case themes.responsiveTheme:
+          this.themeService.updateTheme(themes.lightTheme)
+          break;
+        default:
+          break;
+      }
+    })
+  }
+
+  setTheme(theme) {
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList
+    const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) => item.endsWith('Theme'))
+    if (themeClassesToRemove.length) {
+      overlayContainerClasses.remove(...themeClassesToRemove)
+    }
+    overlayContainerClasses.add(theme)
+    this.componentCssClass = theme
+  }
+
+  ngOnDestroy() {
+    this.subscriptions.forEach(sub => sub.unsubscribe())
+  }
+
+  showShortcuts() {
+    if (!this.shortcutsDialog) {
+      this.shortcutsDialog = this.dialog.open(KeyboardShortcutsComponent, {
+        data: this.shortcutsDisplay
+      })
+    } else {
+      this.shortcutsDialog.close()
+      this.shortcutsDialog = null
+    }
+  }
+
+  back() {
+    this.location.back()
+  }
+
+  forward() {
+    this.location.forward()
+  }
+
+}
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,59 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { HttpClientModule } from '@angular/common/http';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { FlexLayoutModule } from '@angular/flex-layout'
+
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatButtonModule } from '@angular/material/button'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
+import { MatIconModule } from '@angular/material/icon'
+import { MatTableModule } from '@angular/material/table'
+import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatDividerModule } from '@angular/material/divider'
+import { MatTooltipModule } from '@angular/material/tooltip'
+import { MatDialogModule } from '@angular/material/dialog'
+import { MatAutocompleteModule } from '@angular/material/autocomplete'
+
+import { ItemsComponent } from './items/items.component';
+import { AddItemComponent } from './add-item/add-item.component';
+import { EditItemComponent } from './edit-item/edit-item.component';
+import { AddTransactionComponent } from './add-transaction/add-transaction.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { KeyboardShortcutsComponent } from './keyboard-shortcuts/keyboard-shortcuts.component';
+import { ItemSearchComponent } from './item-search/item-search.component';
+import { AddItemModalComponent } from './add-item/add-item-modal/add-item-modal.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ConfigurationComponent,
+    ItemsComponent,
+    AddItemComponent,
+    EditItemComponent,
+    AddTransactionComponent,
+    TransactionsComponent,
+    KeyboardShortcutsComponent,
+    ItemSearchComponent,
+    AddItemModalComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule,
