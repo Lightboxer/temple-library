feec6f962a11a6c7886aafa85cebd984792f8fe3
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-06-04 14:04:17 +0000

    Added Logo

--- frontend/src/app/configuration/configuration.component.spec.ts
+++ frontend/src/app/configuration/configuration.component.spec.ts
@@ -1,1 +1,5 @@
-import { ComponentFixture, 
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ConfigurationComponent } from './configuration.component';
+
+describe('ConfigurationComponent
