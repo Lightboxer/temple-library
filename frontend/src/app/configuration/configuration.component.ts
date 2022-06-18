9533777f48524992cebd5cbdd59ca4bcb0e2eab4
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-06-18 18:30:21 +0000

    fix copyright year

--- frontend/src/app/configuration/configuration.component.ts
+++ frontend/src/app/configuration/configuration.component.ts
@@ -86,3 +86,116 @@ export class ConfigurationComponent implements OnInit, OnDestroy {
       this.submitting = true
       this.apiService.configure(this.configForm.controls['host'].value?.trim(), this.configForm.controls['dataDir'].value?.trim(), this.configForm.controls['password'].value).then(() => {
         this.submitting = false
+        this.apiService.host = this.configForm.controls['host'].value?.trim()
+        this.apiService.dataDir = this.configForm.controls['dataDir'].value?.trim()
+        this.apiService.rememberPassword = this.configForm.controls['rememberPassword'].value
+        this.apiService.password = this.configForm.controls['password'].value
+        this.errorService.showSimpleSnackBar('Configuration saved')
+        this.router.navigate(['/home'])
+      }).catch(err => {
+        this.submitting = false
+        this.errorService.showError(err)
+      })
+    }
+  }
+
+  changePassword() {
+    if (this.changePasswordForm.valid && this.changePasswordForm.controls['newPassword'].value.length > this.apiService.minPasswordLength && this.configForm.controls['password'].valid) {
+      this.submitting = true
+      this.apiService.changePassword(this.configForm.controls['password'].value, this.changePasswordForm.controls['newPassword'].value).then(() => {
+        this.submitting = false
+        this.apiService.password = this.changePasswordForm.controls['newPassword'].value
+        this.errorService.showSimpleSnackBar('Password changed')
+        this.router.navigate(['/home'])
+      }).catch(err => {
+        this.submitting = false
+        this.errorService.showError(err)
+      })
+    }
+  }
+
+  onItemsFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.importForm.patchValue({
+        items: file
+      });
+    }
+  }
+
+  onTransactionsFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.importForm.patchValue({
+        transactions: file
+      });
+    }
+  }
+
+  readFile(blob: Blob): Promise<string> {
+    return new Promise((resolve, reject) => {
+      let reader = new FileReader()
+      reader.readAsText(blob)
+      reader.onload = () => {
+        resolve(reader.result.toString())
+      }
+    })
+  }
+
+  downloadJSONObject(object: any, fileName: string) {
+    const str = JSON.stringify(object)
+    const bytes = new TextEncoder().encode(str)
+    const blob = new Blob([bytes], {
+      type: "application/json;charset=utf-8"
+    })
+    let url = window.URL.createObjectURL(blob);
+    let a = document.createElement('a');
+    document.body.appendChild(a);
+    a.setAttribute('style', 'display: none');
+    a.href = url;
+    a.download = fileName;
+    a.click();
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
+
+  async export() {
+    try {
+      let items = await this.apiService.items()
+      let transactions = await this.apiService.transactions()
+      this.downloadJSONObject(items, 'items.json')
+      this.downloadJSONObject(transactions, 'transactions.json')
+    } catch (err) {
+      this.errorService.showError(err)
+    }
+  }
+
+  async importFiles() {
+    if (this.importForm.valid && confirm('Import these data files? Any existing data will be irreversibly replaced.')) {
+      let items = []
+      let transactions = []
+      try {
+        items = JSON.parse(await this.readFile(this.importForm.get('items').value))
+        transactions = JSON.parse(await this.readFile(this.importForm.get('items').value))
+      } catch (err) {
+        this.errorService.showError(err)
+      }
+      this.submitting = true
+      this.apiService.importData(items, transactions).then(() => {
+        this.submitting = false
+        this.errorService.showSimpleSnackBar('Data Imported')
+        this.router.navigate(['/home'])
+      }).catch(err => {
+        this.submitting = false
+        this.errorService.showError(err)
+      })
+    }
+  }
+
+  back() {
+    this.location.back()
+  }
+
+  getMinPasswordLength() {
+    return this.apiService.minPasswordLength
+  }
