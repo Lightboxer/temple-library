7e6f2f1d5659ab53eed346a3dec188fd9f934a67
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-06-29 19:44:08 +0000

    * bug fix

--- frontend/src/app/edit-item/edit-item.component.spec.ts
+++ frontend/src/app/edit-item/edit-item.component.spec.ts
@@ -4,4 +4,8 @@ import { EditItemComponent } from './edit-item.component';
 
 describe('EditItemComponent', () => {
   let component: EditItemComponent;
-  
+  let fixture: ComponentFixture<EditItemComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      de
