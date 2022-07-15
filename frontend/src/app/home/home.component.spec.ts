877f5416ed7264271e7dfbb91097fee4a44d2647
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-07-15 07:56:02 +0000

    UBI: fix autoresize handling in R/O mode

--- frontend/src/app/home/home.component.spec.ts
+++ frontend/src/app/home/home.component.spec.ts
@@ -1,1 +1,6 @@
-import { ComponentFixture, TestBed } fro
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { HomeComponent } from './home.component';
+
+describe('HomeComponent', () => {
+  let component: Ho
