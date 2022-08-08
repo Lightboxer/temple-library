4bc0f36ee2c3a9019a4fe0ef8f9b4c462bb9c7fb
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-08-08 22:39:07 +0000

    Refactoring (Rubocop corrections).

--- frontend/src/app/item-search/item-search.component.spec.ts
+++ frontend/src/app/item-search/item-search.component.spec.ts
@@ -1,1 +1,6 @@
-import { ComponentFixture, TestBed } from '@a
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItemSearchComponent } from './item-search.component';
+
+describe('ItemSearchComponent', () => {
+  let component
