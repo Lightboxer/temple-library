8c9e6293af450e38f2326a04d2d1124c59896c1a
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-08-13 22:28:45 +0000

    Small optimizations

--- frontend/src/app/item-search/item-search.component.spec.ts
+++ frontend/src/app/item-search/item-search.component.spec.ts
@@ -3,4 +3,9 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemSearchComponent } from './item-search.component';
 
 describe('ItemSearchComponent', () => {
-  let component
+  let component: ItemSearchComponent;
+  let fixture: ComponentFixture<ItemSearchComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+     
