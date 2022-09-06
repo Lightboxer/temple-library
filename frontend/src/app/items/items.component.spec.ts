c86b21d6a96d3638d0b4212f24aa4750ddfb1387
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-09-06 17:40:00 +0000

    asix: Do full reset during ax88772_bind

--- frontend/src/app/items/items.component.spec.ts
+++ frontend/src/app/items/items.component.spec.ts
@@ -3,4 +3,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ItemsComponent } from './items.component';
 
 describe('ItemsComponent', () => {
-  let component:
+  let component: ItemsComponent;
+  let fixture: ComponentFixture<ItemsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingM
