790f9797bc652a4d39c82c527044f52032531b1d
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-09-12 15:57:49 +0000

    tiny changes

--- frontend/src/app/items/items.component.ts
+++ frontend/src/app/items/items.component.ts
@@ -2,4 +2,6 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { Item } from 'tinystock-models';
-import { Location 
+import { Location } from '@angular/common';
+import { ApiService } from '../services/api.service';
+import { ErrorService } from '../
