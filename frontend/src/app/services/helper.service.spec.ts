460c6ee15c15c9d3ed8349f87f9bca760b7356d0
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-10-12 06:52:43 +0000

    Autotranslated

--- frontend/src/app/services/helper.service.spec.ts
+++ frontend/src/app/services/helper.service.spec.ts
@@ -1,3 +1,9 @@
 import { TestBed } from '@angular/core/testing';
 
-import { HelperService } from './helper.service'
+import { HelperService } from './helper.service';
+
+describe('HelperService', () => {
+  let service: HelperService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
