1b72802d659644af45f1a3274d790c51b3dc3923
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-10-24 13:28:50 +0000

    Incorporated review comments

--- frontend/src/app/services/helper.service.ts
+++ frontend/src/app/services/helper.service.ts
@@ -37,4 +37,7 @@ export class HelperService {
   commafy(num: number) {
     let str = num.toString().split('.')
     if (str[0].length >= 4)
-      str[0] = str[0].replace(/(\d
+      str[0] = str[0].replace(/(\d)(?=(\d{3})+$)/g, '$1,')
+    if (str[1] && str[1].length >= 4)
+      str[1] = str[1].replace(/(\d{3})/g, '$1 ')
+    return s
