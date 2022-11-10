c24d6f4183ef9ac58f363ece8f15c98295834f21
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-11-10 20:52:08 +0000

    Updated Amharic translation.

--- frontend/src/app/transactions/transactions.component.spec.ts
+++ frontend/src/app/transactions/transactions.component.spec.ts
@@ -1,3 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { TransactionsComponent } from './trans
+import { TransactionsComponent } from './transactions.component';
+
+describe('TransactionsComponent', () => {
+  let component: TransactionsComponent;
+  let fixture: 
