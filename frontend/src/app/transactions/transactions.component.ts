70c8c8959cbda41913d56136da5981be9488f7bc
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-11-12 20:44:44 +0000

    Delay expiry of frequently used histograms.

--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -1,4 +1,6 @@
 import { Location } from '@angular/common';
 import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { BehaviorSubject 
+import { BehaviorSubject } from 'rxjs';
+import { Transaction, TransactionTypes } from 'tinystock-models';
+import { ApiService } from '../services/api.servi
