310ea6d5d6cad0ae450d10e2a48571dbac2d0191
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2022-11-18 10:36:44 +0000

    doap: update URLs

--- frontend/src/app/transactions/transactions.component.ts
+++ frontend/src/app/transactions/transactions.component.ts
@@ -3,4 +3,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
 import { Transaction, TransactionTypes } from 'tinystock-models';
-import { ApiService } from '../services/api.servi
+import { ApiService } from '../services/api.service';
+import { ErrorService } from '../services/error.service';
+import { HelperService } from '../services/helper.service';
+
