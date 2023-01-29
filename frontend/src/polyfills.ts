05cddd90ca6b84d98c0d0b5fd4115d30c3ca811b
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2023-01-29 21:13:22 +0000

    push code

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -7,4 +7,6 @@
  *   2. Application imports. Files imported after ZoneJS that should be loaded before your main
  *      file.
  *
- * The current setup is for so-called "evergreen" brows
+ * The current setup is for so-called "evergreen" browsers; the last versions of browsers that
+ * automatically update themselves. This includes Safari >= 10, Chrome >= 55 (including Opera),
+ * Edge >= 13 
