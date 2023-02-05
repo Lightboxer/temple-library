0117e13c15718651b240f3de9de47c56e940a41f
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2023-02-05 12:57:42 +0000

    Linux 3.10.78

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -18,4 +18,8 @@
  * BROWSER POLYFILLS
  */
 
-/** IE10 and IE11 requires the follow
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
+// import 'classlist.js';  // Run `npm install --save classlist.js`.
+
+/**
+ * We
