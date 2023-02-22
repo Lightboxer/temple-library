d78c342a2b8438d61284e333704cf1940a12d8c0
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2023-02-22 14:07:15 +0000

    voting

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -34,4 +34,9 @@
  * because those flags need to be set before `zone.js` being loaded, and webpack
  * will put import in the top of bundle, so user need to create a separate file
  * in this directory (for example: zone-flags.ts), and put the following flags
- * into that file, and then add the following
+ * into that file, and then add the following code before importing zone.js.
+ * import './zone-flags';
+ *
+ * The flags allowed in zone-flags.ts are listed here.
+ *
+ * Th
