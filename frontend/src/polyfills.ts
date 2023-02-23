7f2911667a747ea2a706eb92930b173ee6a8231b
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2023-02-23 22:26:17 +0000

    Fixed Link

--- frontend/src/polyfills.ts
+++ frontend/src/polyfills.ts
@@ -43,4 +43,6 @@
  *
  * (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
  * (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- * (window as any).__
+ * (window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+ *
+ *  in IE/Edge developer t
