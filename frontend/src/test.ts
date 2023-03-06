448c8b3fd71766df432d8663f6153ef0b1af2e88
Lightboxer <113272421+Lightboxer@users.noreply.github.com>
2023-03-06 14:21:32 +0000

    Removed commented out code.

--- frontend/src/test.ts
+++ frontend/src/test.ts
@@ -5,4 +5,7 @@ import { getTestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
-} from '@angular/p
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: {
+  context(path: string, deep?: boolean, filter?: RegEx
